931a9a4a7ca3ac173b2ba7cca027c4366b0d3b96
Jibin <jibinjoseph.dev18@gmail.com>
2024-04-19 14:44:57 -0600

    Note Database - Room setup

---
+++ app/src/main/java/com/jibin/mvvmcleanarchitecturenotesapp/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,14 @@
+package com.jibin.mvvmcleanarchitecturenotesapp.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.jibin.mvvmcleanarchitecturenotesapp.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabase: RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+}
