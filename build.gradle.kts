721092620a23e62e2abd4e178ca67a7a15e9f3a5
Jibin <jibinjoseph.dev18@gmail.com>
2024-04-19 14:45:52 -0600

    gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:7.0.4")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+    }
+}
